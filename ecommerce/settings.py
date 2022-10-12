4b0ecfca1baeed037a4cd23bec946f7f2c343a40
Vikas <vikas1618072@gmail.com>
2022-10-12 10:58:26 +0800

    connected with database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'UHy8Ee6PUyRhXJxPPH9r',
+        'HOST':'containers-us-west-84.railway.app',
+        'PORT':'7793',
     }
 }
 
